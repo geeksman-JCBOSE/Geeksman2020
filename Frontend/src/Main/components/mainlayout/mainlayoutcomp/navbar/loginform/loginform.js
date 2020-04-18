de33c575a4c8174ed82d78c61f6c26e83897fd45
Aayush <aayushtyagi2020@gmail.com>
2020-04-18 18:26:58 +0530

    Login Form

--- Frontend/src/Main/components/mainlayout/mainlayoutcomp/navbar/loginform/loginform.js
+++ Frontend/src/Main/components/mainlayout/mainlayoutcomp/navbar/loginform/loginform.js
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./loginform.css";
-import { Link, Navlink } from "react-router-dom";
-class LoginForm extends React.Component {
-  componentDidMount() {}
+import { Modal, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+function LoginForm() {
+  const [show, setShow] = useState(false);
 
-  render() {
-    return (
-      <div>
-        <div className="loginbtn">
-          <Link to="#">Login</Link>
-        </div>
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <div className="loginbtn">
+        <Link to="#" onClick={handleShow}>
+          Login
+        </Link>
       </div>
-    );
-  }
-}
 
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <span className="lead">Login</span>
+        </Modal.Header>
+
+        <Modal.Body>
+          <div class="form">
+            <form class="login-form">
+              <input type="text" placeholder="username" />
+              <input type="password" placeholder="password" />
+              <button>login</button>
+            </form>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <i class="fab fa-instagram social"></i>
+          <i class="fab fa-facebook social"></i>
+          <i class="fab fa-github social"></i>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
 export default LoginForm;
