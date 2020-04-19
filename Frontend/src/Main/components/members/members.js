140aecbfb8894728caac8c73d7822625cbac431a
Aayush <aayushtyagi2020@gmail.com>
2020-04-19 19:48:04 +0530

    members error

--- Frontend/src/Main/components/members/members.js
+++ Frontend/src/Main/components/members/members.js
@@ -5,85 +5,79 @@ import back from "./memtop.png";
 import { Link } from "react-router-dom";
 import $ from "jquery";
 
-function Members() {
-  $.ajax({
-    type: "GET",
-    crossDomain: true,
-    dataType: "json",
-    url: "https://geeksman.herokuapp.com/api/members/",
-    headers: {},
-  }).done(function (data) {
-    var obj = JSON.parse(JSON.stringify(data));
-    console.log(data);
-    var details = "";
-    var mdetails = "";
-    for (var i = 0; i < obj.length; i++) {
-      if (
-        obj[i].status === "jsec" ||
-        obj[i].status === "seceratory" ||
-        obj[i].status === "sessionhead" ||
-        obj[i].status === "smediahead"
-      ) {
-        details +=
-          `
+class Members extends React.Component {
+  componentDidMount() {
+    $.ajax({
+      type: "GET",
+      crossDomain: true,
+      dataType: "json",
+      url: "https://geeksman.herokuapp.com/api/members/",
+      headers: {},
+    }).done(function (data) {
+      var obj = JSON.parse(JSON.stringify(data));
+      console.log(data);
+      var details = "";
+      for (var i = 0; i < obj.length; i++) {
+        if (obj[i].status == "jsec" || obj[i].status == "seceratory") {
+          details +=
+            `
         <div class="col-sm-4 margincards">
             <div class="dcards">
               <div class="dcard">
                 <div class="dimg">
                   <img src="` +
-          obj[i].pic +
-          `" class="dimgstyle" />
+            obj[i].pic +
+            `" class="dimgstyle" />
                 </div>
                 <h1 class="position">` +
-          obj[i].status +
-          `</h1>
+            obj[i].status +
+            `</h1>
                 <div class="aboutd">
                   <span class="dname">` +
-          obj[i].user.first_name +
-          obj[i].user.last_name +
-          `</span>
+            obj[i].user.first_name +
+            obj[i].user.last_name +
+            `</span>
                   <span class="dskill">` +
-          obj[i].tagline +
-          `</span>
+            obj[i].tagline +
+            `</span>
                 </div>
-
                 <div class="hovermaterial">
                   <div class="memberrelativwrapper">
                     <img src="` +
-          obj[i].pic +
-          `" class="dimagestyleflip" />
+            obj[i].pic +
+            `" class="dimagestyleflip" />
                     <div class="membersname">` +
-          obj[i].user.first_name +
-          obj[i].user.last_name +
-          `</div>
+            obj[i].user.first_name +
+            obj[i].user.last_name +
+            `</div>
                     <div class="membersline">` +
-          obj[i].status +
-          `</div>
+            obj[i].status +
+            `</div>
                     <div class="year">` +
-          obj[i].year +
-          ` Year</div>
-                    <div class="ddescription">
+            obj[i].year +
+            ` Year</div>
+                    <div class="description">
                       ` +
-          obj[i].description +
-          `
+            obj[i].description +
+            `
                     </div>
                     <div class="address">` +
-          obj[i].address +
-          `</div>
+            obj[i].address +
+            `</div>
                     <div class="icons">
                       <Link to="` +
-          obj[i].github +
-          `">
+            obj[i].github +
+            `">
                         <i class="fab fa-github"></i>
                       </Link>
                       <Link to="` +
-          obj[i].linkedin +
-          `">
+            obj[i].linkedin +
+            `">
                         <i class="fab fa-linkedin"></i>
                       </Link>
                       <Link to="` +
-          obj[i].user.email +
-          `">
+            obj[i].user.email +
+            `">
                         <i class="fas fa-envelope-open-text"></i>
                       </Link>
                     </div>
@@ -93,65 +87,64 @@ function Members() {
             </div>
           </div>
           
-
     `;
-      } else {
-        mdetails +=
-          `   <div class="col-sm-3 margincardmembers">
+        } else {
+          details +=
+            `   <div class="col-sm-3 margincardmembers">
             <div class="dcards">
               <div class="dmcard">
                 <div class="dimg">
                   <img src="` +
-          obj[i].pic +
-          `" class="dimgstyle" />
+            obj[i].pic +
+            `" class="dimgstyle" />
                 </div>
                 <h1 class="mposition">member</h1>
                 <div class="aboutd">
                   <span class="dname">` +
-          obj[i].user.first_name +
-          obj[i].user.last_name +
-          `</span>
+            obj[i].user.first_name +
+            obj[i].user.last_name +
+            `</span>
                   <span class="dskill">` +
-          obj[i].tagline +
-          `</span>
+            obj[i].tagline +
+            `</span>
                 </div>
                 <div class="hovermaterial">
                   <div class="memberrelativwrapper">
                     <img src="` +
-          obj[i].pic +
-          `" class="dimagestyleflip" />
+            obj[i].pic +
+            `" class="dimagestyleflip" />
                     <div class="membersname">` +
-          obj[i].user.first_name +
-          obj[i].user.last_name +
-          `</div>
+            obj[i].user.first_name +
+            obj[i].user.last_name +
+            `</div>
                     <div class="membersline">` +
-          obj[i].tagline +
-          `</div>
-                    <div class="myear">` +
-          obj[i].year +
-          ` Year</div>
+            obj[i].tagline +
+            `</div>
+                    <div class="year">` +
+            obj[i].year +
+            `</div>
                     <div class="description">
                     ` +
-          obj[i].description +
-          `
+            obj[i].description +
+            `
                     </div>
                     <div class="address">` +
-          obj[i].address +
-          `</div>
+            obj[i].address +
+            `</div>
                     <div class="icons">
                       <Link to="` +
-          obj[i].github +
-          `">
+            obj[i].github +
+            `">
                         <i class="fab fa-github"></i>
                       </Link>
                       <Link to="` +
-          obj[i].linkedin +
-          `">
+            obj[i].linkedin +
+            `">
                         <i class="fab fa-linkedin"></i>
                       </Link>
                       <Link to="` +
-          obj[i].user.email +
-          `">
+            obj[i].user.email +
+            `">
                         <i class="fas fa-envelope-open-text"></i>
                       </Link>
                     </div>
@@ -160,31 +153,32 @@ function Members() {
               </div>
             </div>
           </div>`;
+        }
       }
-    }
 
-    $("#Jsecdetails").append(details);
-    $("#MembersDetails").append(mdetails);
-  });
+      $("#Jsecdetails").append(details);
+    });
+  }
 
-  return (
-    <div className="Members">
-      <Navbar />
-      <div className="memtopback">
-        <img src={back} className="memtopbackstyle" />
-        <div className="memtop-text">
-          <span className="memtop-header">Our Team</span>
-          <span className="memtop-tagline">
-            Our dedicated and hardwording team
-          </span>
+  render() {
+    return (
+      <div className="Members">
+        <Navbar />
+        <div className="memtopback">
+          <img src={back} className="memtopbackstyle" />
+          <div className="memtop-text">
+            <span className="memtop-header">Our Team</span>
+            <span className="memtop-tagline">
+              Our dedicated and hardwording team
+            </span>
+          </div>
+        </div>
+        <div className="jumbotron">
+          <div className="row" id="Jsecdetails"></div>
         </div>
       </div>
-      <div className="jumbotron">
-        <div className="row" id="Jsecdetails"></div>
-        <div className="row" id="MembersDetails"></div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Members;
