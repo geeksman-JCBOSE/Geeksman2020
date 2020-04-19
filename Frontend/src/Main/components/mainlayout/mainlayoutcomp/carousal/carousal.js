46fc1b407b39ca2658055fadc8715ba8b548ae25
parag thakur <paragthakur56@gmail.com>
2020-04-19 20:50:26 +0530

    mainpage

--- Frontend/src/Main/components/mainlayout/mainlayoutcomp/carousal/carousal.js
+++ Frontend/src/Main/components/mainlayout/mainlayoutcomp/carousal/carousal.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "./carousal.css";
+import video from './video.mp4'
 class Carousal extends React.Component {
   componentDidMount() {}
 
   render() {
-    return <div>carousal</div>;
+    return (
+    <div className="carousel">
+   <div className="videotext">
+   <video autoplay loop muted controls="auto" >
+     <source type="video/mp4" src={video}></source>
+   </video>
+   <h2 className="videohead">
+     GEEKSMAN
+   </h2>
+
+   </div> 
+    
+    
+    </div>);
   }
 }
 
--- Frontend/src/Main/components/mainlayout/mainlayoutcomp/footer/footer.js
+++ Frontend/src/Main/components/mainlayout/mainlayoutcomp/footer/footer.js
@@ -1,10 +1,74 @@
-import React from "react";
-import "./footer.css";
+  
+import React from 'react'
+import ReactDom from 'react-dom'
+import image from './logo.png'
+import './footer.css'
+import {Link} from 'react-router-dom'
 class Footer extends React.Component {
   componentDidMount() {}
 
   render() {
-    return <div>footer</div>;
+    return (
+    <div className="footer">
+        
+        
+    <footer className="footer">
+    <div className="footer__addr">
+
+    <img src={image} className="ficon" id="fcusticon1" />
+    
+<h2>Contact</h2>
+
+<address>
+  JCBUST, Sector-6, Faridabad, Haryana, India<br/>
+  7206233857<br />
+    aayushtyagionline@gmail.com
+
+</address>
+</div>
+
+<ul className="footer__nav">
+<li className="nav__item">
+    <h1 className="footer__logo">Suscribe Us</h1>
+       <Link to="#" className="footer__btn">Email Us</Link>
+    <div className="fcontacticons"> 
+      <Link to="#"><i class="fab fa-facebook"></i></Link>
+      <Link to="#"><i class="fab fa-instagram"></i></Link>
+      <Link to="#"><i class="fab fa-github"></i></Link>
+    </div>
+</li>
+
+
+    
+  
+
+<li className="nav__item">
+  <h2 className="nav__title">Links</h2>
+  
+  <ul className="nav__ul">
+    <li>
+      <a href="#">Home</a>
+    </li>
+    
+    <li>
+      <a href="#">Events</a>
+    </li>
+    
+    <li>
+      <a href="#">Members</a>
+    </li>
+  </ul>
+</li>
+</ul>
+
+<div className="legal">
+<p>&copy; 2020 Geeksman. All rights reserved.</p>
+
+</div>
+</footer>
+    
+    
+    </div>)
   }
 }
 
