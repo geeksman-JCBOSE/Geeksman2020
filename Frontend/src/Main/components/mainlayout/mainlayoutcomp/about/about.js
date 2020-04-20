29530fc8201a6aa8a5778eb6081b0889a7ee750c
Aayush <aayushtyagi2020@gmail.com>
2020-04-20 17:30:02 +0530

    mail layout

--- Frontend/src/Main/components/mainlayout/mainlayoutcomp/about/about.js
+++ Frontend/src/Main/components/mainlayout/mainlayoutcomp/about/about.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./about.css";
+
 import {Link} from 'react-router-dom'
 class About extends React.Component {
   componentDidMount() {}
@@ -18,24 +19,31 @@ class About extends React.Component {
    }
   render() {
     return <div>
-   
     <div className="about">
     <div className="aboutopborder"></div>
      <ul className="firstlist">
      <li><i class="fab fa-node-js"></i></li>
+     <li><i class="fab fa-twitter"></i></li>
      <li><i class="fab fa-css3-alt"></i></li>
      <li><i class="fab fa-angular"></i></li>
      <li><i class="fab fa-html5"></i></li>
      </ul>
      <div className="secondlist">
       <blockquote>Geeksman is a perfect blend of hardworking and focussed humans</blockquote>
-      <Link to="" className="secondlistbtn" onMouseEnter={this.handlemouseenter}>OUR TEAM</Link>
+      <div className="second2">
+      <li><i class="fab fa-angular"></i></li>
+   
+      <Link to="#" className="secondlistbtn" onMouseEnter={this.handlemouseenter}>OUR TEAM</Link>
+      <li><i class="fab fa-html5"></i></li>
+      </div>
+     
      </div>
      <ul className="thirdlist">
        <li><i class="fab fa-python"></i></li>
        <li><i class="fab fa-react"></i></li>
        <li><i class="fab fa-android"></i></li>
        <li><i class="fab fa-js"></i></li>
+       <li><i class="fab fa-apple"></i></li>
      </ul>
     </div>
     
--- Frontend/src/Main/components/mainlayout/mainlayoutcomp/carousal/carousal.js
+++ Frontend/src/Main/components/mainlayout/mainlayoutcomp/carousal/carousal.js
@@ -1,10 +1,29 @@
 import React from "react";
 import "./carousal.css";
+// import cover from './cover.mp4'
 class Carousal extends React.Component {
   componentDidMount() {}
 
+
+
   render() {
-    return <div>carousal</div>;
+    return (
+    <div className="carousel">
+    <div className="carouswr">
+    <div className="videobox">
+   <video src="" muted loop="true" autoplay="true" type="mp4" >
+   </video>
+   <div className="textBox">
+     Geeksman <span>Video Text</span>
+   </div>
+
+   </div> 
+
+    </div>
+
+    
+    
+    </div>);
   }
 }
 
