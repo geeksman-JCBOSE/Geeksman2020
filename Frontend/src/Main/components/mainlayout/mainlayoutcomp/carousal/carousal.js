f468d815e99dda8725fd86238ec62b060963fc48
parag thakur <paragthakur56@gmail.com>
2020-04-21 02:49:04 +0530

    carousel video

--- Frontend/src/Main/components/mainlayout/mainlayoutcomp/carousal/carousal.js
+++ Frontend/src/Main/components/mainlayout/mainlayoutcomp/carousal/carousal.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./carousal.css";
-import cover from './cover.mp4'
+import pexels from './pexels.mp4'
 class Carousal extends React.Component {
   componentDidMount() {}
 
@@ -11,7 +11,7 @@ class Carousal extends React.Component {
     <div className="carousel">
     <div className="carouswr">
     <div className="videobox">
-   <video src={cover} muted loop="true" autoplay="true" type="mp4" >
+   <video src={pexels} muted loop="true" autoplay="true" type="mp4" >
    </video>
    <div className="textBox">
      Geeksman <span>We Code Therefore We Are</span>
